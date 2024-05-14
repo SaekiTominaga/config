7542591012a635d14a3a63df6d24e283070ea01d
SaekiTominaga <kcsu3470+github@gmail.com>
2024-05-14 12:45:55 +0900

    ルールの順序を公式サイトの記載順序に合わせる

--- packages/stylelint/stylelint.config.js
+++ packages/stylelint/stylelint.config.js
@@ -1,24 +1,88 @@
 export default {
 	extends: ['stylelint-config-standard', 'stylelint-config-concentric-order'],
 	rules: {
-		'at-rule-empty-line-before': [
-			'always',
-			{
-				except: ['blockless-after-same-name-blockless', 'first-nested'],
-				ignore: ['after-comment'],
-				ignoreAtRules: ['import'],
-			},
-		],
-		'block-no-empty': null,
+		/**
+		 * Avoid errors
+		 */
+		/* Descending */
+		'no-descending-specificity': null, // ✅
+
+		/* Duplicate */
+		// ✅ declaration-block-no-duplicate-custom-properties
+		// ✅ declaration-block-no-duplicate-properties
+		// ✅ font-family-no-duplicate-names
+		// ✅ keyframe-block-no-duplicate-selectors
+		// ✅ no-duplicate-at-import-rules
+		// ✅ no-duplicate-selectors
+
+		/* Empty */
+		'block-no-empty': null, // ✅
+		// ✅ comment-no-empty
+		// ✅ no-empty-source
+
+		/* Invalid */
+		// ✅ color-no-invalid-hex
+		// ✅ function-calc-no-unspaced-operator
+		// ✅ keyframe-declaration-no-important
+		// ✅ media-query-no-invalid
+		// ✅ named-grid-areas-no-invalid
+		// ✅ no-invalid-double-slash-comments
+		// ✅ no-invalid-position-at-import-rule
+		// ✅ string-no-newline
+
+		/* Irregular */
+		// ✅ no-irregular-whitespace
+
+		/* Missing */
+		// ✅ custom-property-no-missing-var-function
+		// ✅ font-family-no-missing-generic-family-keyword
+
+		/* Non-standard */
+		// ✅ function-linear-gradient-no-nonstandard-direction
+
+		/* Overrides */
+		// ✅ declaration-block-no-shorthand-property-overrides
+
+		/* Unmatchable */
+		// ✅ selector-anb-no-unmatchable
+
+		/* Unknown */
+		// ✅ annotation-no-unknown
+		// ✅ at-rule-no-unknown
+		'declaration-property-value-no-unknown': true,
+		// ✅ function-no-unknown
+		// ✅ media-feature-name-no-unknown
+		'media-feature-name-value-no-unknown': true,
+		// no-unknown-animations
+		// no-unknown-custom-media
+		// no-unknown-custom-properties
+		// ✅ property-no-unknown
+		// ✅ selector-pseudo-class-no-unknown
+		// ✅ selector-pseudo-element-no-unknown
+		// ✅ selector-type-no-unknown
+		// ✅ unit-no-unknown
+
+		/**
+		 * Enforce conventions
+		 */
+		/* Allowed, disallowed & required */
+		// at-rule-allowed-list
+		// at-rule-disallowed-list
+		// ✅ at-rule-no-vendor-prefix
+		// at-rule-property-required-list
 		'color-hex-alpha': 'never',
 		'color-named': 'never',
-		'custom-property-pattern': '^_?([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+		// color-no-hex
+		// comment-word-disallowed-list
 		'declaration-no-important': [
 			true,
 			{
 				severity: 'warning',
 			},
 		],
+		// declaration-property-unit-allowed-list
+		// declaration-property-unit-disallowed-list
+		// declaration-property-value-allowed-list
 		'declaration-property-value-disallowed-list': [
 			{
 				display: [
@@ -114,10 +178,18 @@ export default {
 				severity: 'warning',
 			},
 		],
-		'declaration-property-value-no-unknown': true,
+		// function-allowed-list
+		// function-disallowed-list
 		'function-url-no-scheme-relative': true,
-		'media-feature-name-value-no-unknown': true,
-		'no-descending-specificity': null,
+		// function-url-scheme-allowed-list
+		// function-url-scheme-disallowed-list
+		// length-zero-no-unit
+		// media-feature-name-allowed-list
+		// media-feature-name-disallowed-list
+		// ✅ media-feature-name-no-vendor-prefix
+		// media-feature-name-unit-allowed-list
+		// media-feature-name-value-allowed-list
+		// property-allowed-list
 		'property-disallowed-list': [
 			[
 				'top',
@@ -166,13 +238,27 @@ export default {
 				severity: 'warning',
 			},
 		],
-		'selector-max-id': 0,
-		'selector-max-universal': [
-			1,
-			{
-				ignoreAfterCombinators: ['+'],
-			},
-		],
+		// ✅ property-no-vendor-prefix
+		// rule-selector-property-disallowed-list
+		// selector-attribute-name-disallowed-list
+		// selector-attribute-operator-allowed-list
+		// selector-attribute-operator-disallowed-list
+		// selector-combinator-allowed-list
+		// selector-combinator-disallowed-list
+		// selector-disallowed-list
+		// selector-no-qualifying-type
+		// ✅ selector-no-vendor-prefix
+		// selector-pseudo-class-allowed-list
+		// selector-pseudo-class-disallowed-list
+		// selector-pseudo-element-allowed-list
+		// selector-pseudo-element-disallowed-list
+		// unit-allowed-list
+		// unit-disallowed-list
+		// ✅ value-no-vendor-prefix
+
+		/* Case */
+		// ✅ function-name-case
+		// ✅ selector-type-case
 		'value-keyword-case': [
 			'lower',
 			{
@@ -180,6 +266,78 @@ export default {
 			},
 		],
 
+		/* Empty lines */
+		'at-rule-empty-line-before': [
+			'always',
+			{
+				except: ['blockless-after-same-name-blockless', 'first-nested'],
+				ignore: ['after-comment'],
+				ignoreAtRules: ['import'],
+			},
+		], // ✅
+		// ✅ comment-empty-line-before
+		// ✅ custom-property-empty-line-before
+		// ✅ declaration-empty-line-before
+		// ✅ rule-empty-line-before
+
+		/* Max & min */
+		// ✅ declaration-block-single-line-max-declarations
+		// declaration-property-max-values
+		// max-nesting-depth
+		// ✅ number-max-precision
+		// selector-max-attribute
+		// selector-max-class
+		// selector-max-combinators
+		// selector-max-compound-selectors
+		'selector-max-id': 0,
+		// selector-max-pseudo-class
+		// selector-max-specificity
+		// selector-max-type
+		'selector-max-universal': [
+			1,
+			{
+				ignoreAfterCombinators: ['+'],
+			},
+		],
+		// time-min-milliseconds
+
+		/* Notation */
+		// ✅ alpha-value-notation
+		// ✅ color-function-notation
+		// ✅ color-hex-length
+		// font-weight-notation
+		// ✅ hue-degree-notation
+		// ✅ import-notation
+		// ✅ keyframe-selector-notation
+		// ✅ lightness-notation
+		// ✅ media-feature-range-notation
+		// ✅ selector-not-notation
+		// ✅ selector-pseudo-element-colon-notation
+
+		/* Pattern */
+		// comment-pattern
+		// ✅ custom-media-pattern
+		'custom-property-pattern': '^_?([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+		// ✅ keyframes-name-pattern
+		// ✅ selector-class-pattern
+		// ✅ selector-id-pattern
+		// selector-nested-pattern
+
+		/* Quotes */
+		// ✅ font-family-name-quotes
+		// ✅ function-url-quotes
+		// ✅ selector-attribute-quotes
+
+		/* Redundant */
+		// ✅ declaration-block-no-redundant-longhand-properties
+		// ✅ shorthand-property-no-redundant-values
+
+		/* Whitespace inside */
+		// ✅ comment-whitespace-inside
+
+		/**
+		 * order
+		 */
 		'order/order': [
 			[
 				'custom-properties',

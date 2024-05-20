bf600c34914b747b60222d06859e86491c0e4d8b
SaekiTominaga <kcsu3470+github@gmail.com>
2024-05-20 12:07:59 +0900

    index.js → eslint.config.js

--- packages/eslint/eslint.config.js
+++ packages/eslint/eslint.config.js
@@ -1,3 +1,91 @@
-import config from './index.js';
+// @ts-check
 
-export default config;
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintJs from '@eslint/js';
+import pluginJsdoc from 'eslint-plugin-jsdoc';
+import pluginTypeScript from '@typescript-eslint/eslint-plugin';
+import configEslintLayoutFormatting from './rules/eslint/layout&formatting.js';
+import configEslintPossibleProblems from './rules/eslint/possible-problems.js';
+import configEslintSuggestions from './rules/eslint/suggestions.js';
+import configImport from './rules/import.js';
+import configJsdoc from './rules/jsdoc.js';
+
+const compat = new FlatCompat();
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+	eslintJs.configs.recommended,
+	...compat.config({ extends: 'eslint-config-airbnb-base' }),
+	configEslintPossibleProblems,
+	configEslintSuggestions,
+	configEslintLayoutFormatting,
+	configImport,
+	pluginJsdoc.configs['flat/recommended'],
+	configJsdoc,
+	{
+		languageOptions: {
+			ecmaVersion: 'latest', // TODO: デフォルト値は latest だが、明示的に指定しないと Top-level await で Parsing error が発生する
+			parserOptions: {
+				project: true,
+			},
+		},
+		plugins: {
+			pluginJsdoc,
+		},
+	},
+	{
+		files: ['*.ts'],
+		rules: {
+			...pluginTypeScript.configs['strict-type-checked'].rules,
+			...pluginTypeScript.configs['stylistic-type-checked'].rules,
+			...pluginJsdoc.configs['flat/recommended-typescript'].rules,
+			'dot-notation': 'off',
+			'@typescript-eslint/dot-notation': 'off',
+			'@typescript-eslint/no-extraneous-class': 'off',
+			'@typescript-eslint/no-non-null-assertion': 'off',
+			'@typescript-eslint/no-unsafe-argument': 'off',
+			'@typescript-eslint/no-unsafe-assignment': 'off',
+			'@typescript-eslint/no-unsafe-call': 'off',
+			'@typescript-eslint/no-unsafe-member-access': 'off',
+			'@typescript-eslint/no-unsafe-return': 'off',
+			'@typescript-eslint/strict-boolean-expressions': [
+				'error',
+				{
+					allowNullableBoolean: true,
+				},
+			],
+		},
+	},
+	{
+		files: ['*.d.ts'],
+		rules: {
+			'no-use-before-define': 'off',
+			'no-var': 'off',
+			'vars-on-top': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+		},
+	},
+	{
+		files: ['*.test.ts', '*.test.js'],
+		rules: {
+			'no-new': 'off',
+			'no-tabs': 'off',
+			'no-unused-expressions': 'off',
+		},
+	},
+	{
+		files: ['*.test.ts'],
+		rules: {
+			'@typescript-eslint/ban-ts-comment': 'off',
+			'@typescript-eslint/no-non-null-assertion': 'off',
+		},
+	},
+	{
+		files: ['*.test.js'],
+		rules: {
+			'import/no-extraneous-dependencies': 'off',
+			'import/no-named-as-default': 'off',
+			'import/no-named-as-default-member': 'off',
+		},
+	},
+];

736e6c5565fa420236d2b981219714bbad58f22e
SaekiTominaga <kcsu3470+github@gmail.com>
2024-05-29 19:52:49 +0900

    Markuplint のテストを Node.js API 使用に変更

---
+++ packages/markuplint/__tests__/index.test.js
@@ -0,0 +1,51 @@
+import { strict as assert } from 'node:assert/strict';
+import { test } from 'node:test';
+import fs from 'node:fs';
+import { MLEngine } from 'markuplint';
+import config from '../markuplint.config.js';
+
+test('valid', async (t) => {
+	let linterResults = [];
+	t.beforeEach(async () => {
+		const files = fs.promises.glob('__tests__/valid/**/*.html');
+
+		for await (const file of files) {
+			console.debug(file);
+			const engine = new MLEngine(await MLEngine.toMLFile(file.replaceAll('\\', '/')), {
+				config: config,
+			});
+
+			linterResults.push(await engine.exec());
+		}
+	});
+
+	await t.test('violations', () => {
+		assert.equal(
+			linterResults.every((result) => result.violations.length === 0),
+			true,
+		);
+	});
+});
+
+test('invalid', async (t) => {
+	let linterResults = [];
+	t.beforeEach(async () => {
+		const files = fs.promises.glob('__tests__/invalid/**/*.html');
+
+		for await (const file of files) {
+			console.debug(file);
+			const engine = new MLEngine(await MLEngine.toMLFile(file.replaceAll('\\', '/')), {
+				config: config,
+			});
+
+			linterResults.push(await engine.exec());
+		}
+	});
+
+	await t.test('violations', () => {
+		assert.equal(
+			linterResults.every((result) => result.violations.length >= 1),
+			true,
+		);
+	});
+});

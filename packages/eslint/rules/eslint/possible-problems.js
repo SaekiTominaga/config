f739d1cebc08fff643af1d4b54588c970678cbd6
SaekiTominaga <kcsu3470+github@gmail.com>
2024-07-28 08:39:54 +0900

    eslint-config-airbnb-base; error を移行

--- packages/eslint/rules/eslint/possible-problems.js
+++ packages/eslint/rules/eslint/possible-problems.js
@@ -9,7 +9,9 @@ export default {
 				allowImplicit: true,
 			},
 		], // eslint-config-airbnb-base@15.0.0; best-practices
-		'no-await-in-loop': 'error',
+		'no-await-in-loop': 'error', // eslint-config-airbnb-base@15.0.0; error
+		'no-cond-assign': ['error', 'always'], // eslint-config-airbnb-base@15.0.0; error
+		'no-console': 'warn', // eslint-config-airbnb-base@15.0.0; error
 		'no-constructor-return': 'error', // eslint-config-airbnb-base@15.0.0; best-practices
 		'no-irregular-whitespace': [
 			'error',
@@ -19,7 +21,16 @@ export default {
 				skipTemplates: true,
 			},
 		],
+		'no-promise-executor-return': 'error', // eslint-config-airbnb-base@15.0.0; error
 		'no-self-compare': 'error', // eslint-config-airbnb-base@15.0.0; best-practices
+		'no-template-curly-in-string': 'error', // eslint-config-airbnb-base@15.0.0; error
 		'no-unmodified-loop-condition': 'error',
+		'no-unsafe-optional-chaining': [
+			'error',
+			{
+				disallowArithmeticOperators: true,
+			},
+		], // eslint-config-airbnb-base@15.0.0; error
+		'valid-typeof': ['error', { requireStringLiterals: true }], // eslint-config-airbnb-base@15.0.0; error
 	},
 };

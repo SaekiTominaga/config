088fe45ee8e63e60ea01f1aa509285499ce4a19b
SaekiTominaga <kcsu3470+github@gmail.com>
2024-07-27 21:34:47 +0900

    ESLint の Layout & Formatting 部分を見直し

--- packages/eslint/rules/eslint/layout&formatting.js
+++ packages/eslint/rules/eslint/layout&formatting.js
@@ -3,34 +3,6 @@
 /** @type {import("eslint").Linter.FlatConfig} */
 export default {
 	rules: {
-		'comma-dangle': ['error', 'always-multiline'],
-		'function-paren-newline': 'off',
-		'implicit-arrow-linebreak': 'off',
-		indent: [
-			'error',
-			'tab',
-			{
-				SwitchCase: 1,
-			},
-		],
-		'max-len': 'off',
-		'newline-per-chained-call': 'off',
-		'no-tabs': [
-			'error',
-			{
-				allowIndentationTabs: true,
-			},
-		],
-		'object-curly-newline': 'off',
-		'operator-linebreak': [
-			'error',
-			'after',
-			{
-				overrides: {
-					'?': 'before',
-					':': 'before',
-				},
-			},
-		],
+		'unicode-bom': 'error',
 	},
 };

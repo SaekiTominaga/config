85e23fa374ffe91aea76da6751b88d6e148ffb3f
SaekiTominaga <kcsu3470+github@gmail.com>
2024-07-28 15:39:51 +0900

    整形

--- packages/eslint/rules/jsdoc.js
+++ packages/eslint/rules/jsdoc.js
@@ -3,6 +3,12 @@
 /** @type {import("eslint").Linter.FlatConfig} */
 export default {
 	rules: {
-		'jsdoc/tag-lines': ['warn', 'any', { startLines: 1 }], // Recommended
+		'jsdoc/tag-lines': [
+			'warn',
+			'any',
+			{
+				startLines: 1,
+			},
+		], // Recommended
 	},
 };

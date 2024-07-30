2cad661283531da32dec1bb7a1b399f497f1e025
SaekiTominaga <kcsu3470+github@gmail.com>
2024-07-30 19:45:16 +0900

    TypeScript 関係の例外処理を極力削減

--- packages/eslint/eslint.config.js
+++ packages/eslint/eslint.config.js
@@ -65,14 +65,7 @@ export default tseslint.config(
 			...pluginJsdoc.configs['flat/recommended-typescript'].rules,
 			'dot-notation': 'off',
 			'import/no-unresolved': 'off',
-			'@typescript-eslint/dot-notation': 'off',
 			'@typescript-eslint/no-extraneous-class': 'off',
-			'@typescript-eslint/no-non-null-assertion': 'off',
-			'@typescript-eslint/no-unsafe-argument': 'off',
-			'@typescript-eslint/no-unsafe-assignment': 'off',
-			'@typescript-eslint/no-unsafe-call': 'off',
-			'@typescript-eslint/no-unsafe-member-access': 'off',
-			'@typescript-eslint/no-unsafe-return': 'off',
 			'@typescript-eslint/strict-boolean-expressions': [
 				'error',
 				{

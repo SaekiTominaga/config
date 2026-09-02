4a06a5d7c514d5ae608820b5e722e5b117835b97
SaekiTominaga <kcsu3470+github@gmail.com>
2026-09-02 10:41:21 +0900

    Change browser settings (`unicorn/filename-case`, `import/unambiguous`) (#253)

--- packages/oxlint/src/configs/browser.ts
+++ packages/oxlint/src/configs/browser.ts
@@ -1,32 +1,35 @@
 import { type OxlintConfig, defineConfig } from 'oxlint';
+import rulesImport from '../rules/import.ts';
+import rulesUnicorn from '../rules/unicorn.ts';
 import base from './base.ts';
 
 const config: OxlintConfig = defineConfig({
 	extends: [base],
 	plugins: ['eslint', 'typescript', 'unicorn', 'oxc', 'import', 'jsdoc', 'promise'],
-	jsPlugins: [
-		{
-			name: 'safely-storage',
-			specifier: 'oxlint-plugin-safely-storage',
-		},
-	],
-	rules: {
-		'safely-storage/try-catch': 'error',
-	},
 	overrides: [
 		{
 			files: ['*'],
+			excludeFiles: ['*.spec.{js,ts}', '*.test.{js,ts}'],
 			env: {
-				browser: true, // Since `env` is not inherited via `extends`, it specified within `override` <https://github.com/oxc-project/oxc/issues/20087>
+				browser: true,
+			},
+			jsPlugins: [
+				{
+					name: 'safely-storage',
+					specifier: 'oxlint-plugin-safely-storage',
+				},
+			],
+			rules: {
+				...rulesImport.browser,
+				...rulesUnicorn.browser,
+				'safely-storage/try-catch': 'error',
 			},
 		},
 		{
 			files: ['*.spec.{js,ts}', '*.test.{js,ts}'],
 			env: {
+				browser: true,
 				node: true,
-			},
-			rules: {
-				'safely-storage/try-catch': 'off',
 			},
 		},
 	],
